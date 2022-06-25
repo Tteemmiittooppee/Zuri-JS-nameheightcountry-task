4bcb5bfc9b2b5794103518f2a57bfc6819ff466e
tteemmiittooppee <taiwotemitope004@gmail.com>
2022-06-24 18:30:39 -0700

    final commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let myName="Temitope Taiwo";
+let myHeight="144.1 cm";
+let myCountry="Nigeria";
+document.write("My Name is "+myName +"<br>"+"My Height is "+myHeight +"<br>"+"I am from "+myCountry );
